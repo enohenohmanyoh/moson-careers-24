5717baa827439f6c72bd53459f02fbf778cc50a8
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-07-31 13:39:50 +0200

    added about

--- prop-app/src/app/app.routes.ts
+++ prop-app/src/app/app.routes.ts
@@ -5,16 +5,18 @@ import { JobDetailComponent } from './pages/home/job-detail/job-detail.component
 import {CourseComponent} from "./pages/course/course.component";
 import {ListCourseComponent} from "./pages/list-course/list-course.component";
 import {ViewCourseComponent} from "./pages/view-course/view-course.component";
+import {AboutComponent} from "./pages/about/about.component";
 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {path: '', component: HomeComponent},
   {path: 'list-jobs', component: JobListComponent},
   {path: 'jobs-details/:reference', component: JobDetailComponent},
   {path: 'courses', component: ListCourseComponent},
-  { path: 'courses/create', component: CourseComponent },
-  { path: 'courses/edit/:courseId', component: CourseComponent },
-  { path: 'courses/view/:courseId', component: ViewCourseComponent }
+  {path: 'courses/create', component: CourseComponent},
+  {path: 'courses/edit/:courseId', component: CourseComponent},
+  {path: 'courses/view/:courseId', component: ViewCourseComponent},
+  {path: 'about', component: AboutComponent},
 
 
 //{ path: '**', redirectTo: '' },
